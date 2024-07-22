d149f7d40a13322770e777ff3e3f3d6b3dc504c1
PParkcode <qazwsxedc1226@gmail.com>
2024-07-22 18:51:00 +0900

    feature: 로그인 기능 구현
    로그인 기능 구현

--- presentation/src/main/java/com/greener/presentation/ui/login/login/LoginFragment.kt
+++ presentation/src/main/java/com/greener/presentation/ui/login/login/LoginFragment.kt
@@ -18,6 +18,10 @@ import com.greener.presentation.databinding.FragmentLoginBinding
 import com.greener.presentation.model.UiState
 import com.greener.presentation.ui.base.BaseFragment
 import com.greener.presentation.ui.main.MainActivity
+import com.kakao.sdk.auth.model.OAuthToken
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -44,6 +48,13 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(
                 Log.e("확인", e.stackTraceToString())
             }
         }
+    val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        if (error != null) {
+            Log.e("확인", "카카오계정으로 로그인 실패", error)
+        } else if (token != null) {
+            Log.i("확인", "카카오계정으로 로그인 성공 ${token.accessToken}")
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -52,13 +63,55 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(
     }
 
     override fun initListener() {
+        val context = requireActivity()
         binding.btnLoginGoogle.setOnClickListener {
             requestGoogleLogin()
         }
+        checkExistUser()
+
+
         binding.btnLoginKakao.setOnClickListener {
 
+            // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(context)) {
+                UserApiClient.instance.loginWithKakaoTalk(context) { token, error ->
+                    if (error != null) {
+                        Log.e("확인", "카카오톡으로 로그인 실패", error)
+
+                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+
+                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                        UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                    } else if (token != null) {
+                        Log.i("확인", "카카오톡으로 로그인 성공 ${token.accessToken}")
+                    }
+                }
+            } else {
+                UserApiClient.instance.loginWithKakaoAccount(context, callback = callback)
+                UserApiClient.instance.me { user, error ->
+                    if (error != null) {
+                        Log.e("확인", "사용자 정보 요청 실패", error)
+                    } else if (user != null) {
+                        Log.i(
+                            "확인", "사용자 정보 요청 성공" +
+                                    "\n회원번호: ${user.id}" +
+                                    "\n이메일: ${user.kakaoAccount?.email}" +
+                                    "\n닉네임: ${user.kakaoAccount?.profile?.nickname}" +
+                                    "\n프로필사진: ${user.kakaoAccount?.profile?.thumbnailImageUrl}"
+                        )
+                        viewModel.setEmail(user.kakaoAccount?.email!!)
+                        viewModel.setPhotoUrl(user.kakaoAccount?.profile?.thumbnailImageUrl!!)
+                        viewModel.setName(user.kakaoAccount?.profile?.nickname!!)
+                        viewModel.setProvider(KAKAO)
+                    }
+                }
+            }
+            getToken()
         }
-        checkExistUser()
     }
 
     private fun checkExistUser() {
